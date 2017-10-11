bbc40adc4510ae7d281c5db63e1c0664b8f020ad
Keifer Sebastian <sebastbk@hotmail.com>
2017-10-11 12:03:17 -0600

    Added copyright text at bottom. Adjusted some css.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,10 @@ import { Component } from '@angular/core';
   template: `
     <app-nav></app-nav>
     <router-outlet></router-outlet>
+    <div class="d-flex justify-content-around font-weight-bold text-secondary mt-3 mb-4">
+      <span>&copy; 2017</span>
+      <span>Keifer Sebastian</span>
+    </div>
   `,
 })
 export class AppComponent  { name = 'Angular'; }
