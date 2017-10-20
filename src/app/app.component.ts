4242de34a1f67fc8ce94b6d827948941b27dac40
Keifer Sebastian <sebastbk@hotmail.com>
2017-10-20 12:23:03 -0600

    Improved layout for home page. Added space for quiz details and a social tab for friends lists.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-    <app-nav></app-nav>
-    <router-outlet></router-outlet>
-    <div class="d-flex justify-content-around font-weight-bold text-secondary mt-3 mb-4">
-      <span>&copy; 2017</span>
-      <span>Keifer Sebastian</span>
+    <div class="d-flex flex-column" style="height: 100vh;"> 
+      <app-nav></app-nav>
+      <div class="col">
+        <router-outlet></router-outlet>
+      </div>
+      <div class="d-flex justify-content-around font-weight-bold text-secondary mb-5">
+        <span>&copy; 2017</span>
+        <span>Keifer Sebastian</span>
+      </div>
     </div>
   `,
 })
